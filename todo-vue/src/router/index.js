f8937196582085b424581f7d9a1071a3a9c243d6
sprtms16 <sprtms16@gmail.com>
2025-05-25 21:13:30 +0900

    edit router path TodoList

--- todo-vue/src/router/index.js
+++ todo-vue/src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import TodoList from "../views/TodoList.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "TodoList",
+    component: TodoList,
   },
   {
     path: "/about",
