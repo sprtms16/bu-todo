f0b5d7ff249b3c39e9028215d0b33fe2dad68da6
sprtms16 <sprtms16@gmail.com>
2025-05-25 20:17:34 +0900

    init project

---
+++ todo-api/src/test/java/com/sprtms16/todo_api/TodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprtms16.todo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
